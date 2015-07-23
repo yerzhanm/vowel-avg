d553bef87e50a4091f8ff9ece4cab1c9d67f83c6
yerzhan mukhamejan <yerzhan.mukhamejan@bee.kz>
2015-07-24 03:48:08 +0600

    task complete

--- src/main/java/kz/luxoft/test/App.java
+++ src/main/java/kz/luxoft/test/App.java
@@ -1,13 +1,96 @@
 package kz.luxoft.test;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import java.io.*;
+import java.util.*;
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Path to input file : ");
+        String s = "";
+        Scanner scanIn = new Scanner(System.in);
+        s = scanIn.nextLine();
+        scanIn.close();
+
+        if(s.equals("")) {
+            System.out.println("Empty file path");
+            return;
+        }
+
+        List<String> words = readWords(s);
+        List<WordSet> wordsAvg = calculateAvg(words);
+        writeToFile(wordsAvg);
+    }
+
+    public static void writeToFile(List<WordSet> wordsAvg){
+        try(PrintWriter writer = new PrintWriter("output.txt", "UTF-8")) {
+            for(WordSet wa : wordsAvg){
+                writer.println(wa);
+            }
+        } catch (FileNotFoundException | UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static List<WordSet> calculateAvg(List<String> words){
+        List<WordSet> wordsAvg = new ArrayList<>();
+
+        for(String w : words){
+            Set<Character> charsSet = new HashSet<Character>();
+            int size = w.length();
+            int vowelsCount = 0;
+            for(char c : w.toCharArray()){
+                if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u') {
+                    charsSet.add(c);
+                    vowelsCount++;
+                }
+            }
+
+            WordSet wordSet = new WordSet();
+            wordSet.charSet=charsSet;
+            wordSet.wordLength=size;
+            wordSet.totalVowel=vowelsCount;
+
+            if(wordsAvg.contains(wordSet)){
+                for (WordSet wa : wordsAvg)
+                    if(wa.equals(wordSet)) {
+                        wa.count++;
+                        wa.totalVowel+=vowelsCount;
+                        break;
+                    }
+            }else{
+                wordsAvg.add(wordSet);
+            }
+        }
+        Collections.sort(wordsAvg);
+        return wordsAvg;
+    }
+
+    public static List<String> readWords(String filePath) {
+        List<String> wordList = new ArrayList<String>();
+
+        try (
+                FileReader fr = new FileReader(filePath);
+                BufferedReader br = new BufferedReader(fr)
+        ){
+
+            String line = br.readLine();
+            while (line != null) {
+                String[] parts = line.split("[^a-zA-Z0-9\\-\']");
+                for (String w : parts) {
+                    if(w.matches("^[a-zA-Z0-9\\-']{1,}$")) {
+                        wordList.add(w.toLowerCase());
+                    }
+                }
+                line = br.readLine();
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return wordList;
     }
 }
--- src/test/java/kz/luxoft/test/AppTest.java
+++ src/test/java/kz/luxoft/test/AppTest.java
@@ -1,38 +1,89 @@
 package kz.luxoft.test;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.*;
+
+public class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+    @Test
+    public void test1(){
+        List<String> words = App.readWords(getClass().getClassLoader().getResource("test-resources/input1.txt").getPath());
+        List<WordSet> wordsAvg = App.calculateAvg(words);
+
+        WordSet ws1 = new WordSet();
+        Set<Character> ch1 = new HashSet<Character>();
+        ch1.add('a');
+        ch1.add('o');
+        ws1.charSet = ch1;
+        ws1.count=2;
+        ws1.totalVowel=5;
+        ws1.wordLength=6;
+
+
+        WordSet ws2 = new WordSet();
+        Set<Character> ch2 = new HashSet<Character>();
+        ch2.add('a');
+        ch2.add('e');
+        ws2.charSet = ch2;
+        ws2.count=1;
+        ws2.totalVowel=2;
+        ws2.wordLength=4;
+
+        WordSet ws3 = new WordSet();
+        Set<Character> ch3 = new HashSet<Character>();
+        ch3.add('a');
+        ch3.add('o');
+        ws3.charSet = ch3;
+        ws3.count=1;
+        ws3.totalVowel=2;
+        ws3.wordLength=5;
+
+        Assert.assertTrue(wordsAvg.contains(ws1));
+        Assert.assertTrue(wordsAvg.contains(ws2));
+        Assert.assertTrue(wordsAvg.contains(ws3));
+        Assert.assertEquals(words.size(), 4);
+        Assert.assertEquals(wordsAvg.size(), 3);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    public void test2(){
+        List<String> words = App.readWords(getClass().getClassLoader().getResource("test-resources/input2.txt").getPath());
+        List<WordSet> wordsAvg = App.calculateAvg(words);
+
+        WordSet ws1 = new WordSet();
+        Set<Character> ch1 = new HashSet<Character>();
+        ch1.add('e');
+        ws1.charSet = ch1;
+        ws1.count=1;
+        ws1.totalVowel=1;
+        ws1.wordLength=4;
+
+        Assert.assertTrue(wordsAvg.contains(ws1));
+        Assert.assertEquals(words.size(), 1);
+        Assert.assertEquals(wordsAvg.size(), 1);
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void test3(){
+        List<String> words = App.readWords(getClass().getClassLoader().getResource("test-resources/input3.txt").getPath());
+        List<WordSet> wordsAvg = App.calculateAvg(words);
+
+        Assert.assertEquals(wordsAvg.get(0).toString(), "({e, o}, 8) -> 3");
+        Assert.assertEquals(wordsAvg.get(1).toString(), "({a, e}, 8) -> 3");
+        Assert.assertEquals(wordsAvg.get(2).toString(), "({e, i}, 7) -> 2");
+        Assert.assertEquals(wordsAvg.get(3).toString(), "({a, e}, 5) -> 2");
+        Assert.assertEquals(wordsAvg.get(4).toString(), "({e}, 3) -> 1");
+        Assert.assertEquals(wordsAvg.get(5).toString(), "({o}, 2) -> 1");
+    }
+
+    @Test
+    public void test4(){
+        List<String> words = App.readWords(getClass().getClassLoader().getResource("test-resources/input4.txt").getPath());
+        Assert.assertTrue(words.isEmpty());
+        List<WordSet> wordsAvg = App.calculateAvg(words);
+        Assert.assertTrue(wordsAvg.isEmpty());
     }
 }
